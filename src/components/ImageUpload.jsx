89c88d77da1ba133fae6717845434de62c51fb41
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-03-14 17:01:09 +0530

    image upload

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,54 +1,4 @@
 
-// import axios from "axios";
-// import { useState } from "react";
-
-// function ImageUpload() {
-//   const [selectedImage, setSelectedImage] = useState(null);
-
-//   const handleImageChange = (e) => {
-//     const file = e.target.files[0];
-//     setSelectedImage(file);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (!selectedImage) {
-//       console.log("No image selected.");
-//       return;
-//     }
-
-   
-//     console.log("Image selected:", selectedImage);
-//     // Clear selected image
-//     setSelectedImage(null);
-//   };
-
-//   return (
-//     <div className="flex justify-center items-center h-screen">
-//       <div className="max-w-md w-full px-4 py-8 bg-white rounded-md shadow-xl">
-//         <h1 className="text-3xl font-bold text-center mb-4">Image Upload</h1>
-//         <form onSubmit={handleSubmit} className="space-y-4">
-//           <input
-//             type="file"
-//             accept="image/*"
-//             onChange={handleImageChange}
-//             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-//           />
-//           <button
-//             type="submit"
-//             className="w-full px-4 py-2 text-white bg-black rounded-md hover:bg-black-600 focus:outline-none focus:bg-black-600"
-//           >
-//             Create QR
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ImageUpload;
-
-
 import { useState } from "react";
 import axios from "axios";
 
@@ -74,11 +24,12 @@ function ImageUpload() {
 
     try {
       const userToken = sessionStorage.getItem('token');
-      const response = await axios.post("https://qr-code-generator-api-tau.vercel.app/user/imageupload", formData, {
+      console.log(userToken)
+      const response = await axios.post("http://localhost:3010/user/imageupload", formData, {
         headers: {
-          "Content-Type": "multipart/form-data",
-          auth: userToken 
-        },
+            "Content-Type": "multipart/form-data",
+            auth: `${userToken}`
+          },
       });
 
       console.log("Images uploaded successfully:", response.data);
@@ -93,7 +44,7 @@ function ImageUpload() {
     <div className="flex justify-center items-center h-screen">
       <div className="max-w-md w-full px-4 py-8 bg-white rounded-md shadow-xl">
         <h1 className="text-3xl font-bold text-center mb-4">Image Upload</h1>
-        <form onSubmit={handleSubmit} className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4" encType={"multipart/form-data"}>
           <input
             name="images"
             type="file"
@@ -102,12 +53,7 @@ function ImageUpload() {
             onChange={handleImageChange}
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
           />
-          <button
-            type="submit"
-            className="w-full px-4 py-2 text-white bg-black rounded-md hover:bg-black-600 focus:outline-none focus:bg-black-600"
-          >
-            Upload Images
-          </button>
+          <button type="submit" className="w-full px-4 py-2 text-white bg-black rounded-md hover:bg-black-600 focus:outline-none focus:bg-black-600"> Upload Images </button>
         </form>
       </div>
     </div>
@@ -120,63 +66,3 @@ export default ImageUpload;
 
 
 
-// import { useState } from "react";
-// import QRCode from "qrcode.react"; // Import QRCode library
-
-// function ImageUpload() {
-//   const [selectedImage, setSelectedImage] = useState(null);
-//   const [qrCodeData, setQRCodeData] = useState(null); // State to hold QR code data
-
-//   const handleImageChange = (e) => {
-//     const file = e.target.files[0];
-//     setSelectedImage(file);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (!selectedImage) {
-//       console.log("No image selected.");
-//       return;
-//     }
-//     // Handle image upload here
-//     console.log("Image selected:", selectedImage);
-//     // Generate QR code data (for demonstration purposes, generating with image name)
-//     const qrData = `Image Name: ${selectedImage.name}`;
-//     setQRCodeData(qrData);
-//     // Clear selected image
-//     setSelectedImage(null);
-//   };
-
-//   return (
-//     <div className="flex flex-col justify-center items-center h-screen">
-//       <div className="max-w-md w-full px-4 py-8 bg-white rounded-md shadow-xl">
-//         <h1 className="text-3xl font-bold text-center mb-4">Image Upload</h1>
-//         <form onSubmit={handleSubmit} className="space-y-4">
-//           <input
-//             type="file"
-//             accept="image/*"
-//             onChange={handleImageChange}
-//             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-//           />
-//           <button
-//             type="submit"
-//             className="w-full px-4 py-2 text-white bg-black rounded-md hover:bg-black-600 focus:outline-none focus:bg-black-600"
-//           >
-//             Upload Image
-//           </button>
-//         </form>
-//       </div>
-//       {/* Display QR code if data is available */}
-//       {qrCodeData && (
-//         <div className="mt-8">
-//           <h2 className="text-2xl font-bold mb-2">QR Code:</h2>
-//           <div className="bg-white p-4 rounded-md shadow-md">
-//             <QRCode value={qrCodeData} />
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default ImageUpload;
