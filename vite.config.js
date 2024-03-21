098dd0d3f3839703312c08312cc2d8e7b474a958
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-03-21 10:49:59 +0530

    add event

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite'
-import React from 'react';
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['axios', 'react', 'react-dom', 'react-qr-code', 'react-router-dom']
-    }
-  }
-})
+  plugins: [react()]
+});
