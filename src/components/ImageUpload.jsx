3eff8f063ed8d8e0a56712557219a29f50cb2f67
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-03-24 10:34:36 +0530

    add event

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -53,7 +53,7 @@ function ImageUpload() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="max-w-md w-full px-4 py-8 bg-white rounded-md shadow-xl">
-        <img src={LOGO_URL} alt="Logo" className="mx-auto mb-4" />
+        <img src={LOGO_URL} alt="Logo" className="mx-auto mb-4 h-28 w-28" />
 
         <h1 className="text-3xl font-bold text-center mb-4">Image Upload</h1>
         <form onSubmit={handleSubmit} className="space-y-4" encType={"multipart/form-data"}>
@@ -68,7 +68,6 @@ function ImageUpload() {
           <button type="submit" className="w-full px-4 py-2 text-white bg-black rounded-md hover:bg-black-600 focus:outline-none focus:bg-black-600"> Upload Images </button>
         </form>
         
-        {/* QR Code */}
         {qrData && (
           <div className="mt-4 justify-center text-center shadow-lg w-36">
             <h2 className="text-xl font-bold mb-2">QR Code:</h2>
